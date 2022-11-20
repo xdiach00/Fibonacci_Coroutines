31f912c45835bc9642166550e96578efda7a55e2
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2022-11-20 20:56:16 +0100

    Rewrite recursion to loop and add yield()

--- src/main/kotlin/Fibonacci.kt
+++ src/main/kotlin/Fibonacci.kt
@@ -3,6 +3,7 @@
     0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, ...
     1  2  3  4  5  6  7   8   9  10  11  12   13   14   15   16   17    18    19    20    21    22     23     24     25      26      27
  */
+import kotlinx.coroutines.yield
 import java.math.BigInteger
 
 /**
@@ -12,8 +13,15 @@ import java.math.BigInteger
  */
 object Fibonacci {
     suspend fun take(n: Int): BigInteger {
-        if (n == 0) return 0.toBigInteger()
-        if (n == 1) return 1.toBigInteger()
-        return take(n - 1) + take(n - 2)
+        var n1 = 0
+        var n2 = 1
+        var res = 0
+        for (k in 2..n) {
+            res = n1 + n2
+            n1 = n2
+            n2 = res
+        }
+       yield()
+        return res.toBigInteger()
     }
 }
