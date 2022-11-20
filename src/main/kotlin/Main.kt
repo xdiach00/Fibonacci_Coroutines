802df73bea1a4bb2f62afa554957479b14d6b28c
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2022-11-20 20:56:58 +0100

    Add coroutines to main function, add two extra threads

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
-fun main() {
+import kotlinx.coroutines.*
+
+fun main(): Unit = runBlocking {
     println("Enter a number:")
     var n = readln().toInt()
     while (1 == 1) {
@@ -9,5 +11,27 @@ fun main() {
         }
     }
 
-    println(countFibonacci(n - 1))
+    try {
+        withTimeout(2000) {
+            CoroutineScope(Dispatchers.Default).launch {
+                println("The $n-th number in the Fibonacci sequence is ${Fibonacci.take(n)}")
+            }
+        }
+    } catch (t: TimeoutCancellationException) {
+        println("The $n-th number in the Fibonacci sequence is too big.")
+    }
+
+    val secondThread = CoroutineScope(Dispatchers.Default).launch {
+        println("This is the second thread. The 8th fibonacci number is ${Fibonacci.take(8)}")
+    }
+
+    val thirdThread = CoroutineScope(Dispatchers.Default).launch {
+        println("This is the third thread. The 9th fibonacci number is ${Fibonacci.take(9)}")
+    }
+
+    launch {
+        while (secondThread.isActive || thirdThread.isActive) {
+            print(".")
+        }
+    }
 }
