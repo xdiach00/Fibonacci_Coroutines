bf67c40dc1f68ee24ec510d2aed921420322cc57
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2022-11-20 20:00:47 +0100

    Add fibonacci to main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    println("Enter a number:")
+    var n = readln().toInt()
+    while (1 == 1) {
+        if (n >= 0) break
+        else {
+            println("Please enter a positive number:")
+            n = readln().toInt()
+        }
+    }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println(countFibonacci(n - 1))
 }
